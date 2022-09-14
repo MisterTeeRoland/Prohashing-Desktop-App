1df9ed05a98f4d86e1e8f60c6f52d7203d608b85
Tyler Roland <tyler.roland92@gmail.com>
2022-09-14 09:50:35 -0400

    Modal to find Prohashing API key

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,80 +5,80 @@ import "../../assets/css/home.css";
 import { Icon } from "coinmarketcap-cryptocurrency-icons";
 
 const Home = ({ settings }) => {
-    const [tokens, setTokens] = useState([]);
+  const [tokens, setTokens] = useState([]);
 
-    useEffect(() => {
-        setTokens([
-            {
-                name: "Bitcoin",
-                symbol: "BTC",
-                balance: 0.0,
-                usdValue: 0.0,
-            },
-            {
-                name: "Ethereum",
-                symbol: "ETH",
-                balance: 0.0,
-                usdValue: 0.0,
-            },
-            {
-                name: "Litecoin",
-                symbol: "LTC",
-                balance: 0.0,
-                usdValue: 0.0,
-            },
-            {
-                name: "Dogecoin",
-                symbol: "DOGE",
-                balance: 0.0,
-                usdValue: 0.0,
-            },
-            {
-                name: "Bitcoin SV",
-                symbol: "BSV",
-                balance: 0.0,
-                usdValue: 0.0,
-            },
-            {
-                name: "Maker",
-                symbol: "MKR",
-                balance: 0.0,
-                usdValue: 0.0,
-            },
-        ]);
-    }, []);
+  useEffect(() => {
+    setTokens([
+      {
+        name: "Bitcoin",
+        symbol: "BTC",
+        balance: 0.0,
+        usdValue: 0.0,
+      },
+      {
+        name: "Ethereum",
+        symbol: "ETH",
+        balance: 0.0,
+        usdValue: 0.0,
+      },
+      {
+        name: "Litecoin",
+        symbol: "LTC",
+        balance: 0.0,
+        usdValue: 0.0,
+      },
+      {
+        name: "Dogecoin",
+        symbol: "DOGE",
+        balance: 0.0,
+        usdValue: 0.0,
+      },
+      {
+        name: "Bitcoin SV",
+        symbol: "BSV",
+        balance: 0.0,
+        usdValue: 0.0,
+      },
+      {
+        name: "Maker",
+        symbol: "MKR",
+        balance: 0.0,
+        usdValue: 0.0,
+      },
+    ]);
+  }, []);
 
-    return (
-        <div className="phContainer">
-            <div className="pageTitle">
-                <FaHome size={"30px"} /> <div className="titleText">Home</div>
+  return (
+    <div className="phContainer">
+      <div className="pageTitle">
+        <FaHome size={"30px"} /> <div className="titleText">Home</div>
+      </div>
+
+      <div className="homeContainer">
+        {tokens.map((token, index) => (
+          <div className="tokenContainer" key={index}>
+            <div className="tokenIcon">
+              <Icon i={token.symbol.toLowerCase()} size={40} />
             </div>
-
-            <div className="homeContainer">
-                {tokens.map((token) => (
-                    <div className="tokenContainer">
-                        <div className="tokenIcon">
-                            <Icon i={token.symbol.toLowerCase()} size={40} />
-                        </div>
-                        <div className="tokenDetails">
-                            <div className="tokenNameBal">
-                                <div className="tokenName">{token.name}</div>
-                                <div className="tokenBalance">
-                                    {token.balance} {token.symbol}
-                                </div>
-                            </div>
-                            <div className="tokenUsdValue">
-                                <div className="tokenCurrencyValue">
-                                    ${token.usdValue.toFixed(2)}
-                                </div>
-                                <div className="tokenCurrency">USD</div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+            <div className="tokenDetails">
+              <div className="tokenNameBal">
+                <div className="tokenName">{token.name}</div>
+                <div className="tokenBalance">
+                  {token.balance} {token.symbol}
+                </div>
+              </div>
+              <div className="tokenUsdValue">
+                <div className="tokenCurrencyValue">
+                  ${token.usdValue.toFixed(2)}
+                </div>
+                <div className="tokenCurrency">USD</div>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,76 +1,89 @@
 import React, { useState } from "react";
-import { FaCog } from "react-icons/fa";
+import { FaCog, FaInfoCircle } from "react-icons/fa";
 import "../../assets/css/layout.css";
 import "../../assets/css/settings.css";
+import ProhashingApiModal from "../Modal/ProhashingApiModal";
 
 const Settings = ({ settings, onSaveSettings }) => {
-    const [apiKey, setApiKey] = useState(settings?.apiKey ?? "");
-    const [currency, setCurrency] = useState(settings?.currency ?? "USD");
-    const [smallBalance, setSmallBalance] = useState(
-        settings?.smallBalance ?? 0.01
-    );
+  const [apiKey, setApiKey] = useState(settings?.apiKey ?? "");
+  const [currency, setCurrency] = useState(settings?.currency ?? "USD");
+  const [smallBalance, setSmallBalance] = useState(
+    settings?.smallBalance ?? 0.01
+  );
 
-    console.log("Settings", settings);
+  const [showPHModal, setShowPHModal] = useState(false);
 
-    const saveSettings = () => {
-        onSaveSettings({ apiKey, currency, smallBalance });
-    };
+  const saveSettings = () => {
+    onSaveSettings({ apiKey, currency, smallBalance });
+  };
 
-    const resetSettings = () => {
-        setApiKey("");
-        setCurrency("USD");
-        setSmallBalance(0.01);
-        onSaveSettings({});
-    };
+  const resetSettings = () => {
+    setApiKey("");
+    setCurrency("USD");
+    setSmallBalance(0.01);
+    onSaveSettings({});
+  };
 
-    return (
-        <div className="phContainer">
-            <div className="pageTitle">
-                <FaCog size={"28px"} />{" "}
-                <div className="titleText">Settings</div>
-            </div>
+  const openPhModal = () => {
+    setShowPHModal(true);
+  };
 
-            <div className="settingsContainer">
-                <div className="formControl">
-                    <label>Prohashing API Key</label>
-                    <input
-                        type="text"
-                        value={apiKey}
-                        onChange={(e) => setApiKey(e.target.value)}
-                    />
-                </div>
+  const closePhModal = () => {
+    setShowPHModal(false);
+  };
 
-                <div className="formControl">
-                    <label>Default Display Currency</label>
-                    <select
-                        value={currency}
-                        onChange={(e) => setCurrency(e.target.value)}
-                    >
-                        <option value="usd">USD</option>
-                    </select>
-                </div>
+  return (
+    <div className="phContainer">
+      <div className="pageTitle">
+        <FaCog size={"28px"} /> <div className="titleText">Settings</div>
+      </div>
 
-                <div className="formControl">
-                    <label>Hide Balances Under</label>
-                    <input
-                        type="number"
-                        value={smallBalance}
-                        onChange={(e) => setSmallBalance(e.target.value)}
-                        min="0"
-                    />
-                </div>
-            </div>
-
-            <div className="buttonContainer">
-                <button className="btnPrimary" onClick={saveSettings}>
-                    Save
-                </button>
-                <button className="btnSecondary" onClick={resetSettings}>
-                    Reset
-                </button>
-            </div>
+      <div className="settingsContainer">
+        <div className="formControl">
+          <label>
+            Prohashing API Key{" "}
+            <FaInfoCircle onClick={openPhModal} style={{ cursor: "pointer" }} />
+          </label>
+          <input
+            type="text"
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
+          />
         </div>
-    );
+
+        <div className="formControl">
+          <label>Default Display Currency</label>
+          <select
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
+          >
+            <option value="usd">USD</option>
+          </select>
+        </div>
+
+        <div className="formControl">
+          <label>Hide Balances Under</label>
+          <input
+            type="number"
+            value={smallBalance}
+            onChange={(e) => setSmallBalance(e.target.value)}
+            min="0"
+          />
+        </div>
+      </div>
+
+      <div className="buttonContainer">
+        <button className="btnPrimary" onClick={saveSettings}>
+          Save
+        </button>
+        <button className="btnSecondary" onClick={resetSettings}>
+          Reset
+        </button>
+      </div>
+
+      {showPHModal && <ProhashingApiModal onClose={closePhModal} />}
+    </div>
+  );
 };
 
 export default Settings;
