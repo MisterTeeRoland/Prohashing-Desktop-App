ac8e65825d5775185927da1ff9703927591f74da
Tyler <tyler.roland92@gmail.com>
2022-09-15 13:14:22 -0400

    Better information modals for workers + earnings

--- src/Components/Modal/EarningsModal.js
+++ src/Components/Modal/EarningsModal.js
@@ -3,15 +3,33 @@ import "../../assets/css/layout.css";
 import "../../assets/css/modal.css";
 
 const EarningsModal = ({ data, onClose }) => {
+    let icon = null;
+    try {
+        icon = require(`../../../node_modules/cryptocurrency-icons/svg/color/${data?.symbol?.toLowerCase()}.svg`);
+        if (icon === undefined) throw new Error();
+    } catch (e) {
+        const genericIcon = require("../../../node_modules/cryptocurrency-icons/svg/color/generic.svg");
+        icon = genericIcon.default;
+    }
+
     return (
         <div className="modalBackground" onClick={onClose}>
             <div className="modal">
                 <div className="modalCloseBox">
                     <div className="modalCloseX">x</div>
                 </div>
-                <div className="modalTitle">Earnings Data</div>
+                <div className="modalTitle">
+                    <img src={icon} alt={data.name} className={"modalIcon"} />
+                    <div>
+                        {data.name}{" "}
+                        <div className="modalSymbol">({data.symbol})</div>
+                    </div>
+                </div>
 
-                <div>{JSON.stringify(data)}</div>
+                <div>Balance: {data.balance}</div>
+                <div>Enabled: {data?.enabled?.toString() ?? false}</div>
+                <div>Algorithm: {data?.algo}</div>
+                <div>Port: {data?.port ?? "null"}</div>
             </div>
         </div>
     );
--- src/Components/Modal/WorkerModal.js
+++ src/Components/Modal/WorkerModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaTruck } from "react-icons/fa";
 import "../../assets/css/layout.css";
 import "../../assets/css/modal.css";
 
@@ -9,9 +10,20 @@ const WorkerModal = ({ data, onClose }) => {
                 <div className="modalCloseBox">
                     <div className="modalCloseX">x</div>
                 </div>
-                <div className="modalTitle">Worker Data</div>
+                <div className="modalTitle">
+                    <FaTruck size={"28px"} style={{ marginRight: "10px" }} />
+                    <div>{data?.miner_name}</div>
+                </div>
 
-                <div>{JSON.stringify(data)}</div>
+                <div>Coin name: {data?.coin_name ?? "null"}</div>
+                <div>Algorithm: {data?.algorithm_name ?? "null"}</div>
+                <div>Difficulty: {data?.difficulty ?? "null"}</div>
+                <div>Hashrate: {data?.hashrate}</div>
+                <div>Hardware: {data?.miner_hardware ?? "null"}</div>
+                <div>
+                    Shares / All Time: {data?.share_count ?? 0} /{" "}
+                    {data?.all_time_share_count ?? 0}
+                </div>
             </div>
         </div>
     );
