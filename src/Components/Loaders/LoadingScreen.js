8621e14d8350d22579cb43c415b69fd960561fd9
Tyler <tyler.roland92@gmail.com>
2022-09-26 12:21:10 -0400

    Small cleanup

--- src/Components/Loaders/LoadingScreen.js
+++ src/Components/Loaders/LoadingScreen.js
@@ -4,11 +4,9 @@ import "../../assets/css/loaders.css";
 
 const LoadingScreen = () => {
     return (
-        <div className="loadingScreen">
-            <div className="loadingContainer">
-                <img className="loadingImg" src={Icon} alt="icon" />
-                <div className="loadingText">LOADING...</div>
-            </div>
+        <div className="loadingContainer">
+            <img className="loadingImg" src={Icon} alt="icon" />
+            <div className="loadingText">LOADING...</div>
         </div>
     );
 };
